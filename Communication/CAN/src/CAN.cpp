15385165b1a782046299c9fd99e01783b0e60bbb
masoares <manueljorgecruz@hotmail.com>
2025-02-26 15:49:46 +0000

    refactor: comment out register write operations in readMessage method

--- Communication/CAN/src/CAN.cpp
+++ Communication/CAN/src/CAN.cpp
@@ -185,7 +185,7 @@ uint8_t CAN::readMessage(uint8_t buffer, uint32_t &can_id, uint8_t *data) {
         printf("\n");
     }
 
-    // this->writeRegister(CANINTF, 0);
+    this->writeRegister(CANINTF, 0);
 	// this->writeRegister(CANINTE, 0x01);
 	// this->writeRegister(RXB0SIDH,0x00);
 	// this->writeRegister(RXB0SIDL,0x60);

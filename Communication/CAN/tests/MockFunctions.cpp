d1346368cfd983ed7d5c6af5819800ec2f97e32d
Rui Pires <ruipedro.pires999@gmail.com>
2025-03-13 14:53:42 +0000

    refactor: update mock functions in CAN tests to suppress unused parameter warnings

--- Communication/CAN/tests/MockFunctions.cpp
+++ Communication/CAN/tests/MockFunctions.cpp
@@ -8,18 +8,26 @@ static std::vector<uint8_t> writtenData;
 static int mockFd = 1;
 
 extern "C" int custom_open(const char* path, int flags) {
+    (void)path;
+    (void)fd;
     return mockFd;
 }
 
 extern "C" int custom_close(int fd) {
+    (void)fd;
     return 0;
 }
 
 extern "C" int custom_ioctl(int fd, unsigned long request, void* arg) {
+    (void)fd;
+    (void)request;
+    (void)arg;
     return 0;
 }
 
 extern "C" ssize_t custom_read(int fd, void* buf, size_t count) {
+    (void)fd;
+
     if (mockData.size() < count) {
         throw std::runtime_error("Not enough mock data available");
     }
@@ -29,6 +37,8 @@ extern "C" ssize_t custom_read(int fd, void* buf, size_t count) {
 }
 
 extern "C" ssize_t custom_write(int fd, const void* buf, size_t count) {
+    (void)fd;
+
     writtenData.insert(writtenData.end(), (uint8_t*)buf, (uint8_t*)buf + count);
     return count;
 }
